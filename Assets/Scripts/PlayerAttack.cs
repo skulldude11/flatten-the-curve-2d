9df950bbd8146614b19b23f7a0c76c5cdb2afd23
skulldude11 <41588958+skulldude11@users.noreply.github.com>
2020-04-08 11:43:45 -0400

    stay safe

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Animator animator;
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+    
+
+    public Transform attackPos;
+    public float attackRange;
+    public LayerMask whatIsEnemies;
+    public int damage;
+   
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        
+    }
+    void Update()
+    {
+        
+        if (timeBtwAttack <= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Breakable>().TakeDamage(damage);
+                }
+            }
+            timeBtwAttack = startTimeBtwAttack;
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+
+       
+
+        
+        
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+
+    }
+}
---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+
+
+    [SerializeField]
+    KeyCode KeyRestart;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyRestart))
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
